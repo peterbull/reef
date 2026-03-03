59f5ee677ab20fadd39ba5ec2dec1893bb084302
peter bull <17122363+peterbull@users.noreply.github.com>
2026-03-03 05:23:06 -0500

    more resolution

--- src/resolver.rs
+++ src/resolver.rs
@@ -22,21 +22,24 @@ impl Resolver {
         }
     }
 
-    pub fn resolve(&mut self, statements: Vec<StmtKind>) {
+    pub fn resolve(&mut self, statements: Vec<StmtKind>) -> Result<(), ReefError> {
         for stmt in statements {
-            self.resolve_stmt(stmt);
+            self.resolve_stmt(stmt)?;
         }
+        Ok(())
     }
 
-    fn resolve_stmt(&mut self, statement: StmtKind) {
+    fn resolve_stmt(&mut self, statement: StmtKind) -> Result<(), ReefError> {
         match statement {
             StmtKind::Block { statements } => {
                 self.begin_scope();
-                self.resolve(statements);
+                self.resolve(statements)?;
                 self.end_scope();
+                Ok(())
             }
             StmtKind::Var { name, initializer } => {
-                self.resolve_var(&name, initializer).unwrap();
+                self.resolve_var_decl(&name, initializer).unwrap();
+                Ok(())
             }
             StmtKind::Function {
                 name,
@@ -45,13 +48,26 @@ impl Resolver {
             } => {
                 self.declare(&name);
                 self.define(&name);
-                self.resolve_fn(parameters, body);
+                self.resolve_fn(parameters, body)?;
+                Ok(())
+            }
+            StmtKind::Expression { expr } => {
+                self.resolve_expr(expr)?;
+                Ok(())
             }
             _ => todo!("finish statement resolutions"),
         }
     }
 
-    fn resolve_var(&mut self, name: &Token, initializer: ExprKind) -> Result<(), ReefError> {
+    fn resolve_expr(&mut self, expression: ExprKind) -> Result<(), ReefError> {
+        match expression.clone() {
+            ExprKind::Variable { name } => self.resolve_var_expr(&name, expression),
+            ExprKind::Assign { name, value } => self.resolve_assignment(&name, *value, expression),
+            _ => todo!("finish expression resolutions"),
+        }
+    }
+
+    fn resolve_var_decl(&mut self, name: &Token, initializer: ExprKind) -> Result<(), ReefError> {
         self.declare(name);
         match initializer {
             ExprKind::None => {}
@@ -60,36 +76,38 @@ impl Resolver {
         self.define(name);
         Ok(())
     }
-    fn resolve_fn(&mut self, parameters: Vec<Token>, body: Vec<StmtKind>) {
+
+    fn resolve_var_expr(&mut self, name: &Token, expression: ExprKind) -> Result<(), ReefError> {
+        if !self.scopes.is_empty() && self.scopes.last().unwrap().get(&name.lexeme) == Some(&false)
+        {
+            return Err(ReefError::reef_general_error(
+                "Can't read local variable in its own initializer.",
+            ));
+        }
+        self.resolve_local(expression, name)?;
+        Ok(())
+    }
+
+    fn resolve_assignment(
+        &mut self,
+        name: &Token,
+        value: ExprKind,
+        expression: ExprKind,
+    ) -> Result<(), ReefError> {
+        self.resolve_expr(value)?;
+        self.resolve_local(expression, name)?;
+        Ok(())
+    }
+
+    fn resolve_fn(&mut self, parameters: Vec<Token>, body: Vec<StmtKind>) -> Result<(), ReefError> {
         self.begin_scope();
         for param in parameters {
             self.declare(&param);
             self.define(&param);
         }
-        self.resolve(body);
+        self.resolve(body)?;
         self.end_scope();
-    }
-
-    fn resolve_expr(&mut self, expression: ExprKind) -> Result<(), ReefError> {
-        match expression.clone() {
-            ExprKind::Variable { name } => {
-                if !self.scopes.is_empty()
-                    && self.scopes.last().unwrap().get(&name.lexeme) == Some(&false)
-                {
-                    return Err(ReefError::reef_general_error(
-                        "Can't read local variable in its own initializer.",
-                    ));
-                }
-                self.resolve_local(expression, &name)?;
-                Ok(())
-            }
-            ExprKind::Assign { name, value } => {
-                self.resolve_expr(*value)?;
-                self.resolve_local(expression, &name)?;
-                Ok(())
-            }
-            _ => todo!("finish expression resolutions"),
-        }
+        Ok(())
     }
 
     fn resolve_local(&mut self, expr: ExprKind, name: &Token) -> Result<(), ReefError> {
